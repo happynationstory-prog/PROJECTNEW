fc99e461c60c69c26194405707ae75a8ffa44936
happynationstory-prog <happynationstory@gmail.com>
2026-04-13 19:55:08 -0300

    Update vite.config.ts

--- PROJECT NEW/app/vite.config.ts
+++ PROJECT NEW/app/vite.config.ts
@@ -13,3 +13,28 @@ export default defineConfig({
     },
   },
 });
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      // Other configs...
+
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      tseslint.configs.stylisticTypeChecked,
+
+      // Other configs...
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      // other options...
+    },
+  },
+])
